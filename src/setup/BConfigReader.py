a325ba4fa086153d992cf2a6cd56b02369e454d6
charan <nagacharan.tangirala@tum.de>
2023-04-11 15:04:13 +0200

    modified the config reader to support multiple formats

---
+++ src/setup/BConfigReader.py
@@ -0,0 +1,134 @@
+import xml.etree.ElementTree as Et
+
+from abc import ABCMeta, abstractmethod
+from os import makedirs
+from os.path import dirname, join, exists
+
+
+class ConfigDict:
+    def __init__(self):
+        self.sensor_params = {}
+        self.node_params = {}
+        self.entity_params = {}
+        self.controller_params = {}
+        self.simulation_params = {}
+
+
+class ConfigReader(metaclass=ABCMeta):
+    def __init__(self, config_file: str):
+        self.config_file = config_file
+        self.project_path = dirname(config_file)
+
+        # Create the objects to read and store input params
+        self.config_dict = ConfigDict()
+
+    def read_config(self):
+        """
+        Read the config file.
+        """
+        config_root = Et.parse(self.config_file).getroot()
+        for child in config_root:
+            if child.tag == 'sensors':
+                self._read_sensors(child.text)
+            elif child.tag == 'nodes':
+                self._read_nodes(child.text)
+            elif child.tag == 'entities':
+                self._read_entities(child.text)
+            elif child.tag == 'control':
+                self._read_control(child.text)
+            elif child.tag == 'simulation':
+                self._read_simulation_params(child)
+            else:
+                raise ValueError('Invalid tag in config file: %s' % child.tag)
+
+    def _read_simulation_params(self, simulation_data: Et.Element):
+        """
+        Read the parameters of the simulation and store them in the config dict.
+
+        Parameters
+        ----------
+        simulation_data : Et.Element
+            The simulation data element from the config file.
+        """
+        sim_params = {}
+        for child in simulation_data:
+            if child.tag == 'step':
+                sim_params['step'] = int(child.text)
+            elif child.tag == 'start':
+                sim_params['start'] = float(child.text)
+            elif child.tag == 'end':
+                sim_params['end'] = float(child.text)
+            elif child.tag == 'seed':
+                sim_params['seed'] = int(child.text)
+            elif child.tag == 'update_step':
+                sim_params['update_step'] = int(child.text)
+            elif child.tag == 'output':
+                sim_params = self._parse_output_params(child, sim_params)
+            else:
+                raise ValueError('Invalid tag in simulation config file: %s' % child.tag)
+
+        # Store the simulation params
+        self.config_dict.simulation_params = sim_params
+
+    @abstractmethod
+    def _read_sensors(self, sensor_file):
+        pass
+
+    @abstractmethod
+    def _read_nodes(self, node_file):
+        pass
+
+    @abstractmethod
+    def _read_entities(self, entity_file):
+        pass
+
+    @abstractmethod
+    def _read_control(self, control_file):
+        pass
+
+    def _parse_output_params(self, child, sim_params):
+        """
+        Parse the output parameters from the config file.
+
+        Parameters
+        ----------
+        child : Et.Element
+            The output element from the config file.
+        sim_params : Dict[str, Any]
+            The simulation parameters parsed from the config file.
+
+        Returns
+        -------
+        Dict[str, Any]
+            The simulation output parameters with the output parameters added.
+        """
+        sim_params['output_dir'] = self._create_output_dir(child.attrib['path'])
+        sim_params['output_step'] = int(child.attrib['step'])
+        sim_params['output_level'] = child.attrib['level']
+        sim_params['output_type'] = child.attrib['type']
+        return sim_params
+
+    def _create_output_dir(self, output_dir: str):
+        """
+        Create the output directory if it does not exist.
+
+        Parameters
+        ----------
+        output_dir : str
+            The relative path to the output directory.
+        """
+        output_dir = join(self.project_path, output_dir)
+        if not exists(output_dir):
+            makedirs(output_dir)
+        return output_dir
+
+    def get_config_dict(self) -> ConfigDict:
+        """
+        Get the config dict.
+
+        Returns
+        -------
+        ConfigDict
+            The config dict.
+        """
+        return self.config_dict
---
+++ src/setup/SConfigHDF5Reader.py
@@ -0,0 +1,70 @@
+import h5py
+
+from os.path import join
+
+from src.setup.BConfigReader import ConfigReader
+
+
+class ConfigHDF5Reader(ConfigReader):
+    def __init__(self, config_file: str):
+        """
+        Initialize the HDF5 config reader.
+        """
+        super().__init__(config_file)
+
+    def _read_sensors(self, sensor_file):
+        """
+        Read the sensor HDF5 file and parse the parameters of the sensors.
+        In our case, the nodes are stored in entities file. So, we do nothing here.
+        """
+        pass
+
+    def _read_nodes(self, node_file):
+        """
+        Read the node HDF5 file and parse the parameters of the nodes.
+        In our case, the nodes are stored in entities file. So, we do nothing here.
+        """
+        pass
+
+    def _read_entities(self, entity_file):
+        """
+        Read the entity HDF5 file and parse the parameters of the entities.
+        """
+        # Get the entities group
+        self.h5_file = h5py.File(join(self.project_path, entity_file), 'r')
+
+        # Get the entities group
+        entities_group = self.h5_file['entities']
+
+        # Read the entities
+        for entity_id, entity in entities_group.items():
+            # Create a dict to store the entity parameters
+            entity_params = {'id': int(entity_id),
+                             'type': entity.attrs['type'],
+                             'start_time': int(entity.attrs['start_time']),
+                             'end_time': int(entity.attrs['end_time']),
+                             'sensors': entity.get('sensor_ids'),
+                             'positions': entity.get('positions')}
+
+            # Add the entity parameters to the config dict
+            self.config_dict.entity_params[int(entity_id)] = entity_params
+
+        # Get the sensors group
+        sensors_group = self.h5_file['sensors']
+
+        # Read the sensors
+        for sensor_id, sensor in sensors_group.items():
+            # Create a dict to store the sensor parameters
+            sensor_params = {'id': int(sensor_id),
+                             'data_size': float(sensor.attrs['data_size']),
+                             'type': sensor.attrs['type'],
+                             'start_time': int(sensor.attrs['start_time']),
+                             'end_time': int(sensor.attrs['end_time']),
+                             'frequency': int(sensor.attrs['frequency']),
+                             'mode': sensor.attrs['mode']}
+
+            # Add the sensor parameters to the config dict
+            self.config_dict.sensor_params[int(sensor_id)] = sensor_params
+
+    def _read_control(self, control_file):
+        pass
