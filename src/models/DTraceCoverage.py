f4a97cad230ec5ca9a05f85f26993e4882c99f96
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-06-13 08:25:37 +0200

    added trace coverage model to read coverage data from parquet files

---
+++ src/models/DTraceCoverage.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from src.models.BCoverage import AgentCoverage
+
+
+class AgentTraceCoverage(AgentCoverage):
+    def __init__(self, agent_id, coverage_data: pd.DataFrame):
+        """
+        Initialize the trace coverage model.
+        """
+        super().__init__(agent_id)
+        self.coverage_data = coverage_data
+
+    def step(self):
+        """
+        Step through the model.
+        """
+        # Check if the coverage data is empty
+        if self.coverage_data is None or len(self.coverage_data) == 0:
+            self.coverage_data = []
+
+        # Get the agents in the coverage
+        self.coverage_data = self.coverage_data[self.coverage_data["time"] == self.current_time].tolist()
