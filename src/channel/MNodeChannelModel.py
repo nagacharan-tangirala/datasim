23555dacfda38aa69c69e13a375fa65584d5c8b6
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-06-13 08:25:02 +0200

    added channel models to handle channels

---
+++ src/channel/MNodeChannelModel.py
@@ -0,0 +1,28 @@
+from mesa import Model
+from mesa.time import BaseScheduler
+
+from src.device.BNode import NodeBase
+
+
+class NodeChannelModel(Model):
+    def __init__(self, nodes: dict[int, NodeBase]):
+        """
+        Initialize the node channel model.
+        """
+        super().__init__()
+
+        self.nodes = nodes
+        self.schedule: BaseScheduler = BaseScheduler(self)
+
+    def add_channel(self, channel):
+        """
+        Add the channel to the model.
+        """
+        self.schedule.add(channel)
+
+    def step(self, *args, **kwargs):
+        """
+        Step through the node channel model.
+        """
+        current_time = args[0]
+        self.schedule.step()
