33860f0dfee01bb8e19ee3db315b99c7f56b2119
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-06-23 10:06:34 +0200

    modified the input parsing code to match new changes

--- src/setup/SSimulationSetup.py
+++ src/setup/SSimulationSetup.py
@@ -1,36 +1,44 @@
 import xml.etree.ElementTree as Et
 from os import makedirs
 from os.path import dirname, exists, join
+from typing import Any, Dict, Tuple
 
 import pyarrow.parquet as pq
 from pandas import DataFrame, read_csv
+
+from src.core.CustomExceptions import InvalidXMLAttributeError, InvalidXMLTagError
 
 
 class SimulationSetup:
     def __init__(self, config_file: str):
         """
         Initialize the config reader.
+
+        Parameters
+        ----------
+        config_file : str
+            The path to the configuration file.
         """
         self.config_file: str = config_file
         self.project_path: str = dirname(config_file)
 
-        # Create the objects to read and store input data
-        self.ue_data: DataFrame = DataFrame()
-        self.coverage_data: DataFrame = DataFrame()
+        self.ue_trace_data: DataFrame = DataFrame()
         self.cell_tower_data: DataFrame = DataFrame()
         self.controller_data: DataFrame = DataFrame()
-        self.nearest_towers_data: DataFrame = DataFrame()
+        self.tower_links_data: DataFrame = DataFrame()
 
-        self.simulation_data: dict = {}
-        self.ue_type_data: list[dict] = []
+        self.channel_model_data: dict = {}
+        self.ue_models_data: dict = {}
+        self.cell_tower_models_data: dict = {}
+        self.controller_models_data: dict = {}
 
         self.cell_tower_link_data: DataFrame = DataFrame()
-        self.ue_link_data: DataFrame = DataFrame()
+        self.ue_links_data: DataFrame = DataFrame()
         self.controller_link_data: DataFrame = DataFrame()
 
-        self.model_data: dict = {}
+        self.simulation_data: dict = {}
 
-    def read_input_file(self):
+    def read_input_file(self) -> None:
         """
         Read the config file.
         """
@@ -40,84 +48,124 @@ class SimulationSetup:
                 self._read_input_files(child)
             elif child.tag == 'simulation':
                 self._read_simulation_settings(child)
-            elif child.tag == 'models':
-                self._read_model_data(child)
+            elif child.tag == 'channels':
+                self._read_all_channel_models(child)
             elif child.tag == 'ues':
-                self._read_ue_data(child)
+                self._read_ue_models(child)
+            elif child.tag == 'cell_towers':
+                self._read_cell_tower_models(child)
+            elif child.tag == 'controllers':
+                self._read_controller_models(child)
             else:
-                raise ValueError('Invalid tag in config file: %s' % child.tag)
+                raise InvalidXMLTagError(child.tag)
 
-    def _read_input_files(self, input_files):
+    def _read_input_files(self, input_files) -> None:
         """
         Read the input files.
+
+        Parameters
+        ----------
+        input_files : Element
+            The input files element.
         """
         for child in input_files:
-            if child.tag == 'trace':
-                self._read_trace(child.text)
-            elif child.tag == 'coverage':
-                self._read_ues_coverage(child.text)
+            if child.tag == 'ue_trace':
+                self._read_ue_trace(child.text)
+            elif child.tag == 'ue_links':
+                self.read_ue_links(child.text)
             elif child.tag == 'cell_towers':
                 self._read_cell_towers(child.text)
-            elif child.tag == 'nearest_towers':
-                self._read_nearest_towers(child.text)
+            elif child.tag == 'tower_links':
+                self._read_tower_links(child.text)
             elif child.tag == 'controllers':
                 self._read_controllers(child.text)
             elif child.tag == 'controller_links':
                 self._read_controller_links(child.text)
             else:
-                raise ValueError('Invalid tag in config file: %s' % child.tag)
+                raise InvalidXMLTagError(child.tag)
 
-    def _read_trace(self, ue_file):
+    def _read_ue_trace(self, ue_file) -> None:
         """
         Read the ues trace data from the parquet file.
+
+        Parameters
+        ----------
+        ue_file : str
+            The path to the ues trace file.
         """
         # Get the ues data as a pandas dataframe
-        self.ue_data = pq.read_table(join(self.project_path, ue_file)).to_pandas()
+        self.ue_trace_data = pq.read_table(join(self.project_path, ue_file)).to_pandas()
 
         # Sort the ues data by ue id and time
-        self.ue_data.sort_values(by=['ue_id', 'time'], inplace=True)
+        self.ue_trace_data.sort_values(by=['vehicle_id', 'time'], inplace=True)
 
-    def _read_ues_coverage(self, coverage_file: str):
+    def read_ue_links(self, ue_links_file: str) -> None:
         """
         Read the coverage data from the parquet file.
+
+        Parameters
+        ----------
+        ue_links_file : str
+            The path to the coverage file.
         """
         # Get the coverage data as a pandas dataframe
-        self.coverage_data = pq.read_table(join(self.project_path, coverage_file)).to_pandas()
+        self.ue_links_data = pq.read_table(join(self.project_path, ue_links_file)).to_pandas()
 
         # Sort the coverage data by ue id and time
-        self.coverage_data.sort_values(by=['vehicle_id', 'time'], inplace=True)
+        self.ue_links_data.sort_values(by=['vehicle_id', 'time'], inplace=True)
 
-    def _read_cell_towers(self, cell_tower_file: str):
+    def _read_cell_towers(self, cell_tower_file: str) -> None:
         """
         Read the cell towers data from the CSV file.
+
+        Parameters
+        ----------
+        cell_tower_file : str
+            The path to the cell towers file.
         """
         # Get the cell towers data as a pandas dataframe
         self.cell_tower_data = read_csv(join(self.project_path, cell_tower_file), dtype={'cell_tower_id': int, 'x': float, 'y': float, 'type': str}, usecols=['cell_tower_id', 'x', 'y', 'type'])
 
-    def _read_controllers(self, controller_file: str):
+    def _read_controllers(self, controller_file: str) -> None:
         """
         Read the controllers data from the CSV file.
+
+        Parameters
+        ----------
+        controller_file : str
+            The path to the controllers file.
         """
         # Get the controllers data as a pandas dataframe
         self.controller_data = read_csv(join(self.project_path, controller_file), dtype={'controller_id': int, 'x': float, 'y': float}, usecols=['controller_id', 'x', 'y'])
 
-    def _read_controller_links(self, link_file: str):
+    def _read_controller_links(self, link_file: str) -> None:
         """
         Read the links data from the links CSV file.
+
+        Parameters
+        ----------
+        link_file : str
+            The path to the links file.
         """
         # Read the links csv file as a pandas dataframe
         self.controller_link_data = read_csv(join(self.project_path, link_file),
                                              dtype={'link_id': int, 'cell_tower': int, 'controller': int, 'bandwidth_in': float, 'bandwidth_out': float},
                                              usecols=['link_id', 'cell_tower', 'controller', 'bandwidth_in', 'bandwidth_out'])
 
-    def _read_nearest_towers(self, nearest_towers_file: str):
+    def _read_tower_links(self, tower_links_file: str) -> None:
         """
-        Read the nearest towers data from the CSV file.
+        Read the tower links data from the CSV file.
+
+        Parameters
+        ----------
+        tower_links_file : str
+            The path to the tower links file.
         """
-        # Get the nearest towers data as a pandas dataframe
-        self.nearest_towers_data = read_csv(join(self.project_path, nearest_towers_file),
-                                            dtype={'time': int, 'vehicle_id': int, 'cell_towers': list[int]},
-                                            usecols=['time_step', 'vehicle_id', 'cell_towers'])
+        # Get the tower links data as a pandas dataframe
+        self.tower_links_data = pq.read_table(join(self.project_path, tower_links_file)).to_pandas()
+
+        # Sort the tower links data by vehicle id and time
+        self.tower_links_data.sort_values(by=['agent_id', 'time'], inplace=True)
 
     def _read_output_settings(self, child):
         """
@@ -135,12 +183,12 @@ class SimulationSetup:
         """
         output_settings = {}
 
-        output_settings['output_dir'] = self._create_output_dir(child.attrib['path'])
+        output_settings['output_dir'] = self._create_output_dir(child.attrib['location'])
         output_settings['output_step'] = int(child.attrib['step'])
         output_settings['output_type'] = child.attrib['type']
         return output_settings
 
-    def _read_simulation_settings(self, simulation_data: Et.Element):
+    def _read_simulation_settings(self, simulation_data: Et.Element) -> None:
         """
         Read the parameters of the simulation and store them in the config dict.
 
@@ -164,12 +212,151 @@ class SimulationSetup:
             elif child.tag == 'output':
                 simulation_settings.update(self._read_output_settings(child))
             else:
-                raise ValueError('Invalid tag in simulation config file: %s' % child.tag)
+                raise InvalidXMLTagError(child.tag)
 
         # Store the simulation settings
         self.simulation_data = simulation_settings
 
-    def _read_model_data(self, model_element: Et.Element):
+    def _read_all_channel_models(self, model_element: Et.Element) -> None:
+        """
+        Read the model data from the config file.
+
+        Parameters
+        ----------
+        model_element : Et.Element
+            The channel data element from the config file.
+        """
+        allowed_device_types = ['ue', 'cell_tower', 'controller']
+        for child in model_element:
+            if child.tag != 'channel':
+                raise InvalidXMLTagError(child.tag)
+
+            parsed_model_data, device_type = self._read_channel_model(child)
+
+            if device_type not in allowed_device_types:
+                raise InvalidXMLAttributeError('device', device_type, allowed_device_types)
+            self.channel_model_data[device_type] = parsed_model_data
+
+    @staticmethod
+    def _read_channel_model(channel_element: Et.Element) -> Tuple[Dict[str, Any], str]:
+        """
+        Read the channel model data from the config file.
+
+        Parameters
+        ----------
+        model_element Et.Element
+            The channel model data element from the config file.
+
+        Returns
+        -------
+        Dict[str, Any]
+            The model data.
+        """
+        channel_data = {}
+
+        # Read attributes
+        channel_data['name'] = channel_element.attrib['name']
+        device_type = channel_element.attrib['device']
+
+        # Read model parameters
+        for child in channel_element:
+            if child.tag != 'parameter':
+                raise InvalidXMLTagError(child.tag)
+            channel_data[child.attrib['name']] = child.attrib['value']
+
+        return channel_data, device_type
+
+    def _read_ue_models(self, ues_element: Et.Element) -> None:
+        """
+        Read the types of UEs from the config file.
+        """
+        for child in ues_element:
+            ue_info = {}
+            if child.tag != 'ue':
+                raise InvalidXMLTagError(child.tag)
+
+            ue_info['id'] = int(child.attrib['id'])
+            ue_info['type'] = child.attrib['type']
+            ue_info['weight'] = child.attrib['weight']
+
+            if ue_info['type'] not in ['vehicle']:
+                raise InvalidXMLAttributeError('type', ue_info['type'], ['vehicle'])
+
+            ue_info['models'] = self._read_all_models(child)
+
+            self.ue_models_data[ue_info['id']] = ue_info
+
+    def _read_cell_tower_models(self, cell_towers_element: Et.Element) -> None:
+        """
+        Read the types of cell towers from the config file.
+
+        Parameters
+        ----------
+        cell_towers_element : Et.Element
+            The element with all the cell tower data from the config file.
+        """
+        for child in cell_towers_element:
+            cell_tower_info = {}
+            if child.tag != 'cell_tower':
+                raise InvalidXMLTagError(child.tag)
+
+            cell_tower_info['id'] = int(child.attrib['id'])
+            cell_tower_info['type'] = child.attrib['type']
+            cell_tower_info['weight'] = child.attrib['weight']
+
+            if cell_tower_info['type'] not in ['basic']:
+                raise InvalidXMLAttributeError('type', cell_tower_info['type'], ['basic'])
+
+            cell_tower_info['models'] = self._read_all_models(child)
+
+            self.cell_tower_models_data[cell_tower_info['id']] = cell_tower_info
+
+    def _read_controller_models(self, controllers_element: Et.Element) -> None:
+        """
+        Read the types of controllers from the config file.
+
+        Parameters
+        ----------
+        controllers_element : Et.Element
+            The element with all the controller data from the config file.
+        """
+        for child in controllers_element:
+            controller_info = {}
+            if child.tag != 'controller':
+                raise InvalidXMLTagError(child.tag)
+
+            controller_info['id'] = int(child.attrib['id'])
+            controller_info['type'] = child.attrib['type']
+            controller_info['weight'] = child.attrib['weight']
+
+            if controller_info['type'] not in ['central']:
+                raise InvalidXMLAttributeError('type', controller_info['type'], ['central'])
+
+            controller_info['models'] = self._read_all_models(child)
+
+            self.controller_models_data[controller_info['id']] = controller_info
+
+    def _read_all_models(self, all_models_element: Et.Element) -> Dict[int, Dict[str, Any]]:
+        """
+        Read the types of models from the config file.
+
+        Parameters
+        ----------
+        all_models_element : Et.Element
+            The element with all the model data from the config file.
+        """
+        all_models_data = {}
+        for model_child in all_models_element:
+            if model_child.tag != 'model':
+                raise InvalidXMLTagError(model_child.tag)
+
+            model_data = self._read_model_data(model_child)
+            all_models_data[model_data['id']] = model_data
+
+        return all_models_data
+
+    @staticmethod
+    def _read_model_data(model_element: Et.Element) -> Dict[str, Any]:
         """
         Read the model data from the config file.
 
@@ -178,81 +365,20 @@ class SimulationSetup:
         model_element : Et.Element
             The model data element from the config file.
         """
-        self.model_data = {'ue': {}, 'cell_tower': {}, 'controller': {}}
-        for child in model_element:
-            if child.tag == 'model':
-                parsed_model_data, device_type = self._read_model(child)
-                if device_type not in self.model_data:
-                    raise ValueError('Invalid device type in model config file: %s' % device_type)
-                if parsed_model_data['id'] in self.model_data[device_type]:
-                    raise ValueError('Duplicate model id: %s' % parsed_model_data['id'])
-                self.model_data[device_type][parsed_model_data['id']] = parsed_model_data
-            else:
-                raise ValueError('Invalid tag in model config file: %s' % child.tag)
-
-    @staticmethod
-    def _read_model(model_element: Et.Element):
-        """
-        Read the model data from the config file.
-
-        Parameters
-        ----------
-        model_element Et.Element
-            The model data element from the config file.
-
-        Returns
-        -------
-        Dict[str, Any]
-            The model data.
-        """
         model_data = {}
-
-        # Read attributes
-        model_data['id'] = model_element.attrib['id']
+        model_data['id'] = int(model_element.attrib['id'])
         model_data['type'] = model_element.attrib['type']
         model_data['name'] = model_element.attrib['name']
-        device_type = model_element.attrib['device']
 
-        # Read model parameters
         for child in model_element:
-            if child.tag == 'parameter':
-                model_data[child.attrib['name']] = child.attrib['value']
-            else:
-                raise ValueError('Invalid tag in model config file: %s' % child.tag)
+            if child.tag != 'parameter':
+                raise InvalidXMLTagError(child.tag)
 
-        return model_data, device_type
+            model_data[child.attrib['name']] = child.attrib['value']
 
-    def _read_ue_data(self, ues_element: Et.Element):
-        """
-        Read the types of UEs from the config file.
-        """
-        self.ue_type_data = []
-        for child in ues_element:
-            ue_type_info = {}
-            if child.tag == 'ue':
-                ue_type_info['id'] = child.attrib['id']
-                ue_type_info['type'] = child.attrib['type']
+        return model_data
 
-                if ue_type_info['type'] not in ['vehicle']:
-                    raise ValueError('Invalid type parameter in ue config file: %s' % ue_type_info['type'])
-
-                for param_child in child:
-                    if param_child.tag == 'parameter':
-                        ue_type_info[param_child.attrib['name']] = param_child.attrib['value']
-                    else:
-                        raise ValueError('Invalid tag in ue config file: %s' % param_child.tag)
-                self.ue_type_data.append(ue_type_info)
-
-        # Validate the UE types
-        for ue_type in self.ue_type_data:
-            if 'id' not in ue_type:
-                raise ValueError('Missing id parameter in ue config file')
-
-        ue_weights = [float(ue_type['weight']) for ue_type in self.ue_type_data]
-        if sum(ue_weights) < 1.0:
-            raise ValueError("The sum of the weights of the ue types must be 1.")
-
-    def _create_output_dir(self, output_dir: str):
+    def _create_output_dir(self, output_dir: str) -> str:
         """
         Create the output directory if it does not exist.
 
