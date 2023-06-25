e5906e225e655650f1dfcb5143007297b805bd13
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-06-25 15:06:40 +0200

    removed channel types to modify the design

--- src/channel/DInfiniteBandwidthCellTowerChannel.py
+++
@@ -1,9 +0,0 @@
-from src.channel.BCellTowerChannel import BaseCellTowerChannel
-
-
-class InfiniteBandwidthCellTowerChannel(BaseCellTowerChannel):
-    def __init__(self):
-        """
-        Initialize the infinite bandwidth node channel.
-        """
-        super().__init__()
