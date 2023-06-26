ba8f06720b41f880bdb6ee3182cb615c8bd9f399
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-06-26 21:56:44 +0200

    added first version of wireless channel model

---
+++ src/channel/BWirelessChannel.py
@@ -0,0 +1,56 @@
+from abc import abstractmethod
+from collections import namedtuple
+
+from mesa import Agent
+from pandas import DataFrame
+
+from src.device.BCellTower import CellTowerBase
+from src.device.BUE import UEBase
+
+
+class WirelessChannelBase(Agent):
+    def __init__(self, cell_towers: dict, ue_links_df: DataFrame, tower_links_df: DataFrame):
+        """
+        Initialize the wireless channel.
+        """
+        super().__init__(0, None)
+
+        self._ues: dict[int, UEBase] = {}
+        self._cell_towers: dict[int, CellTowerBase] = cell_towers
+
+        self._ue_links: DataFrame = ue_links_df
+        self._tower_links: DataFrame = tower_links_df
+
+        self.data_from_ues: dict[int, namedtuple] = {}
+        self.data_from_controller: dict[dict[int, namedtuple]] = {}
+
+        self._current_time: int = -1
+
+    @property
+    def current_time(self) -> int:
+        """Get the time stamp."""
+        return self._current_time
+
+    @current_time.setter
+    def current_time(self, value: int) -> None:
+        """ Set the current time."""
+        self._current_time = value
+
+    @abstractmethod
+    def step(self) -> None:
+        """
+        Step through the ue channel.
+        """
+        pass
+
+    def add_ue(self, ue: UEBase) -> None:
+        """
+        Add an ue to the channel.
+        """
+        self._ues[ue.unique_id] = ue
+
+    def remove_ue(self, ue: UEBase) -> None:
+        """
+        Remove an ue from the channel.
+        """
+        self._ues.pop(ue.unique_id)
