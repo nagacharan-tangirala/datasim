4a46698584dec4b091d07fc210d007c2e9d3183a
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-07-22 19:51:39 +0200

    added app runners to represent application handling in the devices

---
+++ src/application/BaseStationAppRunner.py
@@ -0,0 +1,49 @@
+import logging
+
+from src.application.Payload import VehiclePayload, BaseStationPayload, BaseStationResponse
+from src.device.ComputingHardware import ComputingHardware
+
+logger = logging.getLogger(__name__)
+
+
+class BaseStationAppRunner:
+    def __init__(self, device_id: int, hardware_settings: ComputingHardware):
+        """
+        Initialize the base station application runner model.
+        """
+        self._device_id: int = device_id
+        self._hardware_settings: ComputingHardware = hardware_settings
+
+    @property
+    def device_id(self) -> int:
+        """ Get the device id. """
+        return self._device_id
+
+    def generate_basestation_payload(self,
+                                     current_time: int,
+                                     incoming_data: dict[int, VehiclePayload]) -> BaseStationPayload:
+        """
+        Generate data request by running the applications.
+        """
+        logger.debug(f"Generating base station payload for device {self._device_id} at time {current_time}")
+        base_station_payload_request: BaseStationPayload = BaseStationPayload()
+        for vehicle_id, payload_request in incoming_data.items():
+            base_station_payload_request.cpu_required += payload_request.cpu_required
+            base_station_payload_request.memory_required += payload_request.memory_required
+            base_station_payload_request.gpu_required += payload_request.gpu_required
+            base_station_payload_request.battery_required += payload_request.battery_required
+            base_station_payload_request.storage_required += payload_request.storage_required
+
+            # Collect the uplink and downlink data
+            base_station_payload_request.uplink_data += payload_request.uplink_data
+            base_station_payload_request.sources.append(vehicle_id)
+
+        return base_station_payload_request
+
+    def process_result(self, response: BaseStationResponse) -> None:
+        """
+        Process the result and send it to relevant applications.
+        """
+        # Applications are not interested in the response. They just want to know if the data was sent or not.
+        if response.status:
+            logger.debug(f"Data transfer from {self._device_id} at time {response.timestamp} was successful.")
---
+++ src/application/ControllerAppRunner.py
@@ -0,0 +1,57 @@
+import logging
+
+from src.application.Payload import BaseStationResponse, BaseStationPayload
+from src.device.ComputingHardware import ComputingHardware
+
+logger = logging.getLogger(__name__)
+
+
+class ControllerAppRunner:
+    def __init__(self, device_id: int, computing_hardware: ComputingHardware):
+        """
+        Initialize the controller application runner model.
+
+        Parameters
+        ----------
+        device_id : int
+            The id of the device.
+        computing_hardware : ComputingHardware
+            The hardware settings of the device.
+        """
+        self._device_id: int = device_id
+        self._hardware_settings: ComputingHardware = computing_hardware
+
+    @property
+    def device_id(self) -> int:
+        """ Get the device id. """
+        return self._device_id
+
+    def generate_basestation_response(self,
+                                      current_time: int,
+                                      incoming_data: dict[int, BaseStationPayload]) -> dict[int, BaseStationResponse]:
+        """
+        Generate response by running the applications.
+
+        Parameters
+        ----------
+        current_time : int
+            The current time.
+        incoming_data : dict[int, BaseStationPayload]
+            The incoming data.
+
+        Returns
+        -------
+        dict[int, BaseStationResponse]
+            The response of the controller.
+        """
+        logger.debug(f"Generating base station response for device {self._device_id} at time {current_time}")
+        base_station_responses: dict[int, BaseStationResponse] = {}
+
+        for station_id, base_station_payload in incoming_data.items():
+            response: BaseStationResponse = BaseStationResponse()
+            base_station_responses.status = True
+            base_station_responses.timestamp = current_time
+            base_station_responses.destination_vehicles = base_station_payload.sources
+            base_station_responses[station_id] = response
+
+        return base_station_responses
