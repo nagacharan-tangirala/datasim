26f2381ab899707b55499f956c5c5b794afbd902
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-07-26 22:23:28 +0200

    fixed data types, added methods to ensure that timings are within sim limits

--- src/device/ActivationSettings.py
+++ src/device/ActivationSettings.py
@@ -1,21 +1,29 @@
+from numpy import ndarray
+
+
 class ActivationSettings:
-    def __init__(self, activate_times: list[int], disable_times: list[int], active: bool = False):
+    def __init__(self, activate_times: ndarray, disable_times: ndarray, active: bool = False):
         """
         Initialize the activation settings.
         """
-        self._activate_times: list[int] = activate_times
-        self._disable_times: list[int] = disable_times
+        self._activate_times: ndarray[int] = [int(x) for x in activate_times]
+        self._disable_times: ndarray[int] = [int(x) for x in disable_times]
         self._index: int = 0
         self._active = active
 
     @property
     def start_time(self) -> int:
         """ Get the start time. """
+        if self._index >= len(self._activate_times):
+            return 0
         return self._activate_times[self._index]
 
     @property
     def end_time(self) -> int:
         """ Get the end time. """
+        if self._index >= len(self._activate_times):
+            # Return a very large number to ensure that the device is never deactivated
+            return 100000000
         return self._disable_times[self._index]
 
     @property
