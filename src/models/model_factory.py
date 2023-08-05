baeb5cb49dbf780a84bbc00024f84741abeb7c4b
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-05 15:33:29 +0200

    added factory methods to create the newly added models

--- src/models/model_factory.py
+++ src/models/model_factory.py
@@ -2,26 +2,17 @@ import logging
 
 from pandas import DataFrame
 
-from src.application.application_settings import ApplicationSettings
-from src.application.base_station_app_runner import BaseStationAppRunner
-from src.application.controller_app_runner import ControllerAppRunner
-from src.application.vehicle_app_runner import VehicleAppRunner
-from src.core.constants import (
-    C_MODEL_NAME,
-    C_DATA_PROCESSOR,
-    C_POSITION,
-    C_BASE_STATION_FINDER,
+import src.core.constants as constants
+from src.core.exceptions import ModelTypeNotImplementedError
+from src.models.bs_finder import NearestNBaseStationFinder
+from src.models.collector import ControllerCollector
+from src.models.composer import (
+    VehicleDataComposer,
+    BaseStationDataComposer,
+    ControllerDataComposer,
 )
-from src.core.custom_exceptions import ModelTypeNotImplementedError
-from src.device.computing_hardware import ComputingHardware
-from src.models.data_processor.base_station_data_processor import (
-    BaseStationDataProcessor,
-)
-from src.models.data_processor.controller_data_processor import ControllerDataProcessor
-from src.models.data_processor.vehicle_data_processor import VehicleDataProcessor
-from src.models.finder.base_station_finder import BaseStationFinder
-from src.models.mobility.static_mobility import StaticMobilityModel
-from src.models.mobility.trace_mobility import TraceMobilityModel
+from src.models.mobility import *
+from src.models.simplifier import *
 
 logger = logging.getLogger(__name__)
 
@@ -45,13 +36,13 @@ class ModelFactory:
         model_data : dict
             Dictionary containing all the mobility model data.
         """
-        model_name = model_data[C_MODEL_NAME]
+        model_name = model_data[constants.MODEL_NAME]
         match model_name:
             case "static":
                 logger.debug(
-                    f"Creating static mobility model with position {model_data[C_POSITION]}."
+                    f"Creating static mobility model with position {model_data[constants.POSITION]}."
                 )
-                return StaticMobilityModel(model_data[C_POSITION])
+                return StaticMobilityModel(model_data[constants.POSITION])
             case "trace":
                 logger.debug("Creating trace mobility model.")
                 return TraceMobilityModel()
@@ -59,94 +50,111 @@ class ModelFactory:
                 raise NotImplementedError("Other mobility models are not implemented.")
 
     @staticmethod
-    def create_vehicle_app_runner(
-        vehicle_id: int,
-        application_settings: list[ApplicationSettings],
-        computing_hardware: ComputingHardware,
-    ) -> VehicleAppRunner:
+    def create_vehicle_data_composer(data_composer_data: dict) -> VehicleDataComposer:
         """
-        Create a vehicle application runner.
+        Create the data composer model.
         """
-        logger.debug(f"Creating application runner for vehicle {vehicle_id}.")
-        return VehicleAppRunner(vehicle_id, application_settings, computing_hardware)
+        match data_composer_data[constants.MODEL_NAME]:
+            case "simple":
+                return VehicleDataComposer(data_composer_data)
+            case _:
+                raise ModelTypeNotImplementedError(
+                    constants.DATA_COMPOSER,
+                    data_composer_data[constants.MODEL_NAME],
+                )
 
     @staticmethod
-    def create_basestation_app_runner(
-        station_id: int, computing_hardware: ComputingHardware
-    ) -> BaseStationAppRunner:
+    def create_vehicle_data_simplifier(
+        data_simplifier_data: dict,
+    ) -> VehicleDataSimplifier:
         """
-        Create a base station application runner.
+        Create the data simplifier model.
         """
-        logger.debug(f"Creating application runner for base station {station_id}.")
-        return BaseStationAppRunner(station_id, computing_hardware)
+        match data_simplifier_data[constants.MODEL_NAME]:
+            case "simple":
+                return VehicleDataSimplifier(data_simplifier_data)
+            case _:
+                raise ModelTypeNotImplementedError(
+                    constants.DATA_SIMPLIFIER,
+                    data_simplifier_data[constants.MODEL_NAME],
+                )
 
     @staticmethod
-    def create_controller_app_runner(
-        controller_id: int, computing_hardware: ComputingHardware
-    ) -> ControllerAppRunner:
+    def create_base_station_data_composer(
+        data_composer_data: dict,
+    ) -> BaseStationDataComposer:
         """
-        Create a controller application runner.
+        Create the data composer model.
         """
-        logger.debug(f"Creating application runner for controller {controller_id}.")
-        return ControllerAppRunner(controller_id, computing_hardware)
+        match data_composer_data[constants.MODEL_NAME]:
+            case "simple":
+                return BaseStationDataComposer(data_composer_data)
+            case _:
+                raise ModelTypeNotImplementedError(
+                    constants.DATA_COMPOSER,
+                    data_composer_data[constants.MODEL_NAME],
+                )
+
+    @staticmethod
+    def create_base_station_data_simplifier(
+        data_simplifier_data: dict,
+    ) -> BaseStationDataSimplifier:
+        """
+        Create the data simplifier model.
+        """
+        match data_simplifier_data[constants.MODEL_NAME]:
+            case "simple":
+                return BaseStationDataSimplifier(data_simplifier_data)
+            case _:
+                raise ModelTypeNotImplementedError(
+                    constants.DATA_SIMPLIFIER,
+                    data_simplifier_data[constants.MODEL_NAME],
+                )
+
+    @staticmethod
+    def create_controller_data_composer(
+        data_composer_data: dict,
+    ) -> ControllerDataComposer:
+        """
+        Create the data composer model.
+        """
+        match data_composer_data[constants.MODEL_NAME]:
+            case "simple":
+                return ControllerDataComposer(data_composer_data)
+            case _:
+                raise ModelTypeNotImplementedError(
+                    constants.DATA_COMPOSER,
+                    data_composer_data[constants.MODEL_NAME],
+                )
+
+    @staticmethod
+    def create_controller_collector(
+        controller_collector_data: dict,
+    ) -> ControllerCollector:
+        """
+        Create the controller collector model.
+        """
+        match controller_collector_data[constants.MODEL_NAME]:
+            case "simple":
+                return ControllerCollector()
+            case _:
+                raise ModelTypeNotImplementedError(
+                    constants.CONTROLLER_COLLECTOR,
+                    controller_collector_data[constants.MODEL_NAME],
+                )
 
     @staticmethod
     def create_basestation_finder(
         base_stations: dict, base_station_links_df: DataFrame, model_data: dict
-    ) -> BaseStationFinder:
+    ) -> NearestNBaseStationFinder:
         """
         Create the base station finder.
         """
-        match model_data[C_MODEL_NAME]:
+        match model_data[constants.MODEL_NAME]:
             case "nearest":
                 logger.debug(f"Creating nearest base station finder.")
-                return BaseStationFinder(base_stations, base_station_links_df)
+                return NearestNBaseStationFinder(base_stations, base_station_links_df)
             case _:
                 raise ModelTypeNotImplementedError(
-                    C_BASE_STATION_FINDER, model_data[C_MODEL_NAME]
-                )
-
-    @staticmethod
-    def create_vehicle_data_processor(
-        data_processor_model_data: dict,
-    ) -> VehicleDataProcessor:
-        """
-        Create the vehicle data processor model.
-        """
-        match data_processor_model_data[C_MODEL_NAME]:
-            case "simple":
-                return VehicleDataProcessor(data_processor_model_data)
-            case _:
-                raise ModelTypeNotImplementedError(
-                    C_DATA_PROCESSOR, data_processor_model_data[C_MODEL_NAME]
-                )
-
-    @staticmethod
-    def create_base_station_data_processor(
-        data_processor_model_data: dict,
-    ) -> BaseStationDataProcessor:
-        """
-        Create the base station data processor model.
-        """
-        match data_processor_model_data[C_MODEL_NAME]:
-            case "simple":
-                return BaseStationDataProcessor(data_processor_model_data)
-            case _:
-                raise ModelTypeNotImplementedError(
-                    C_DATA_PROCESSOR, data_processor_model_data[C_MODEL_NAME]
-                )
-
-    @staticmethod
-    def create_controller_data_processor(
-        data_processor_model_data: dict,
-    ) -> ControllerDataProcessor:
-        """
-        Create the controller data processor model.
-        """
-        match data_processor_model_data[C_MODEL_NAME]:
-            case "simple":
-                return ControllerDataProcessor(data_processor_model_data)
-            case _:
-                raise ModelTypeNotImplementedError(
-                    C_DATA_PROCESSOR, data_processor_model_data[C_MODEL_NAME]
+                    constants.BASE_STATION_FINDER, model_data[constants.MODEL_NAME]
                 )
