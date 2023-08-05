096c02d60703987bb489b9d6bbf58fb2e3f0fefa
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-05 15:30:07 +0200

    combined network and computing hardware into a single module

--- src/device/hardware.py
+++ src/device/hardware.py
@@ -1,4 +1,4 @@
-from src.application.payload import VehiclePayload
+from src.device.payload import VehiclePayload
 
 
 class ComputingHardware:
@@ -67,3 +67,25 @@ class ComputingHardware:
             and self._battery - payload_request.battery_required >= 0.0
             and self._storage_load + payload_request.storage_required <= self._storage
         )
+
+
+class NetworkHardware:
+    def __init__(self, networking_hardware: dict):
+        self._capacity: float = networking_hardware["capacity"]
+        self._max_connections: int = networking_hardware["max_connections"]
+
+    @property
+    def capacity(self) -> float:
+        """Get the capacity."""
+        return self._capacity
+
+    @property
+    def max_connections(self) -> int:
+        """Get the max connections."""
+        return self._max_connections
+
+    def consume_capacity(self, data_size: float) -> None:
+        """
+        Send data to the base station.
+        """
+        self._capacity -= data_size
