4f9c2b1a9638b6a3fa459f5a63305c4c4960215c
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-05 15:35:31 +0200

    modified controller to use new composer and collector models

--- src/device/controller.py
+++ src/device/controller.py
@@ -4,11 +4,10 @@ from mesa import Agent
 from numpy import ndarray
 from pandas import Series
 
-from src.application.payload import BaseStationPayload, BaseStationResponse
-from src.core.constants import C_MOBILITY_MODEL, C_POSITION, C_DATA_PROCESSOR
-from src.device.activation_settings import ActivationSettings
-from src.device.computing_hardware import ComputingHardware
-from src.device.network_hardware import NetworkHardware
+import src.core.constants as constants
+from src.device.activation import ActivationSettings
+from src.device.hardware import *
+from src.device.payload import BaseStationPayload, BaseStationResponse
 from src.models.model_factory import ModelFactory
 
 logger = logging.getLogger(__name__)
@@ -56,7 +55,9 @@ class CentralController(Agent):
 
         self.processed_base_station_data: dict[int, BaseStationPayload] = {}
 
-        controller_models[C_MOBILITY_MODEL][C_POSITION] = controller_position
+        controller_models[constants.MOBILITY_MODEL][
+            constants.POSITION
+        ] = controller_position
         self._create_models(controller_models)
 
     @property
@@ -107,15 +108,15 @@ class CentralController(Agent):
         """
         model_factory = ModelFactory()
         self._mobility_model = model_factory.create_mobility_model(
-            controller_models[C_MOBILITY_MODEL]
+            controller_models[constants.MOBILITY_MODEL]
         )
-        self._controller_data_processor = (
-            model_factory.create_controller_data_processor(
-                controller_models[C_DATA_PROCESSOR]
-            )
+
+        self._controller_collector = model_factory.create_controller_collector(
+            controller_models[constants.CONTROLLER_COLLECTOR]
         )
-        self._controller_app_runner = model_factory.create_controller_app_runner(
-            self.unique_id, self._computing_hardware
+
+        self._data_composer = model_factory.create_controller_data_composer(
+            controller_models[constants.DATA_COMPOSER]
         )
 
     def use_network_for_uplink(self) -> None:
@@ -143,24 +144,12 @@ class CentralController(Agent):
         self._mobility_model.step()
         self._location = self._mobility_model.current_location
 
-        # Use the data processor to process the data.
-        # self.processed_base_station_data
-        # = self._controller_data_processor.simplify_controller_data(self._received_data)
-
-        # Capture the results based on the base station data.
-        self._controller_app_runner.process_incoming_data(self._received_data)
-
-        logger.debug(
-            f" Time: {self.sim_model.current_time} - "
-            + f"Vehicles: {self._controller_app_runner.vehicle_count} - "
-            + f"Data: {self._controller_app_runner.data_count}."
-        )
+        self._controller_collector.current_time = self.sim_model.current_time
+        self._controller_collector.collect_data(self._received_data)
 
         # Create base station response.
-        self._downlink_response = (
-            self._controller_app_runner.generate_basestation_response(
-                self.sim_model.current_time, self._received_data
-            )
+        self._downlink_response = self._data_composer.generate_basestation_response(
+            self.sim_model.current_time, self._received_data
         )
 
     def downlink_stage(self) -> None:
