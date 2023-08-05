6b5ef17c667fa05ea9ad7c3ae49828533db1c338
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-05 15:34:41 +0200

    removed hardware data from the payload

--- src/device/payload.py
+++ src/device/payload.py
@@ -2,33 +2,34 @@ from dataclasses import dataclass, field
 
 
 @dataclass
+class DataPayload:
+    type: str = ""
+    count: int = 0
+    data_size: float = 0.0
+
+
+@dataclass
 class VehiclePayload:
     source: int = -1
     timestamp: int = -1
-    cpu_required: float = 0.0
-    memory_required: float = 0.0
-    gpu_required: float = 0.0
-    battery_required: float = 0.0
-    storage_required: float = 0.0
-    uplink_data: float = 0.0
+    uplink_data_size: float = 0.0
+    uplink_payload: list[DataPayload] = field(default_factory=lambda: [DataPayload()])
 
 
 @dataclass
 class BaseStationPayload:
-    sources: list[int] = field(default_factory=lambda: [-1])
     timestamp: int = -1
-    cpu_required: float = 0.0
-    memory_required: float = 0.0
-    gpu_required: float = 0.0
-    battery_required: float = 0.0
-    storage_required: float = 0.0
-    uplink_data: list[float] = field(default_factory=lambda: [0.0])
+    uplink_data_size: float = 0.0
+    sources: list[int] = field(default_factory=lambda: [-1])
+    uplink_data: list[VehiclePayload] = field(
+        default_factory=lambda: [VehiclePayload()]
+    )
 
 
 @dataclass
 class VehicleResponse:
-    destination: int = -1
     timestamp: int = -1
+    destination: int = -1
     downlink_data: float = -1
     status: bool = False
 
