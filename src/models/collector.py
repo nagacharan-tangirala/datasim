0344111c9982ffa7bc8df7bee43b3482e50295f1
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-07 11:20:25 +0200

    added stats collection at the controller

--- src/models/collector.py
+++ src/models/collector.py
@@ -6,11 +6,53 @@ class ControllerCollector:
         """
         Initialize the data collector.
         """
-        pass
+        self._total_data_size: float = 0.0
+        self._all_vehicles: list[int] = []
+
+        self._data_types_sizes: dict[str, float] = {}
+
+    @property
+    def total_data_size(self) -> float:
+        """Get the total data size."""
+        return self._total_data_size
+
+    @property
+    def all_vehicles(self) -> list[int]:
+        """Get the list of all vehicles."""
+        return self._all_vehicles
+
+    @property
+    def data_types_sizes(self) -> dict[str, float]:
+        """Get the data types and their sizes."""
+        return self._data_types_sizes
 
     def collect_data(self, incoming_data: dict[int, BaseStationPayload]):
         """
         Collect the data from the controller.
         """
         # Collect the statistics of the incoming data
-        pass
+        self._total_data_size = 0.0
+        self._all_vehicles: list[int] = []
+        self._data_types_sizes = {}
+        for base_station_id, base_station_payload in incoming_data.items():
+            # Total data size
+            self._total_data_size += base_station_payload.uplink_data_size
+            # Collect the vehicle ids.
+            self._all_vehicles.extend(
+                [
+                    vehicle.source
+                    for vehicle in base_station_payload.uplink_data
+                    if vehicle.source != -1
+                ]
+            )
+
+            for vehicle_payload in base_station_payload.uplink_data:
+                if vehicle_payload.source == -1:
+                    continue
+                # Collect the types and their sizes
+                for data_payload in vehicle_payload.uplink_payload:
+                    data_type = data_payload.type
+                    data_size = data_payload.data_size
+                    if data_type not in self._data_types_sizes:
+                        self._data_types_sizes[data_type] = 0
+                    self._data_types_sizes[data_type] += data_size
