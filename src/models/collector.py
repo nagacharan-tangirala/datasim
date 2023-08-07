09529414cc61400b23e56a15737285d156492d8c
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-07 21:34:51 +0200

    simplified data collector after payload changes

--- src/models/collector.py
+++ src/models/collector.py
@@ -34,25 +34,18 @@ class ControllerCollector:
         self._total_data_size = 0.0
         self._all_vehicles: list[int] = []
         self._data_types_sizes = {}
+
         for base_station_id, base_station_payload in incoming_data.items():
-            # Total data size
+            # Calculate total data size and the list of all vehicles
             self._total_data_size += base_station_payload.uplink_data_size
-            # Collect the vehicle ids.
-            self._all_vehicles.extend(
-                [
-                    vehicle.source
-                    for vehicle in base_station_payload.uplink_data
-                    if vehicle.source != -1
-                ]
-            )
+            self._all_vehicles.extend(base_station_payload.sources)
 
             for vehicle_payload in base_station_payload.uplink_data:
-                if vehicle_payload.source == -1:
-                    continue
                 # Collect the types and their sizes
                 for data_payload in vehicle_payload.uplink_payload:
                     data_type = data_payload.type
                     data_size = data_payload.data_size
+
                     if data_type not in self._data_types_sizes:
                         self._data_types_sizes[data_type] = 0
                     self._data_types_sizes[data_type] += data_size
