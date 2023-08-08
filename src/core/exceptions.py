b136affc2d3d172121f1ab6701e41d7a1bd64f9a
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-08 22:28:20 +0200

    removed unused exceptions

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,13 +1,3 @@
-class NoBaseStationsInSimulationError(Exception):
-    """There are no base stations in the simulation."""
-
-    def __init__(self, message: str = ""):
-        super().__init__(message)
-
-    def __str__(self):
-        return f"There are no base stations in the simulation."
-
-
 class UnsupportedInputFormatError(Exception):
     """The input file is not supported."""
 
@@ -28,42 +18,6 @@ class UnsupportedOutputFormatError(Exception):
 
     def __str__(self):
         return f"The output format '{self.output_format}' is not supported."
-
-
-class WrongActivationTimeError(Exception):
-    """The activation time is wrong."""
-
-    def __init__(self, activation_time: int, start_time: int, message: str = ""):
-        super().__init__(message)
-        self.activation_time = activation_time
-        self.start_time = start_time
-
-    def __str__(self):
-        return f"The activation time '{self.activation_time}' is wrong. It should be equal to the start time '{self.start_time}'."
-
-
-class WrongDeactivationTimeError(Exception):
-    """The deactivation time is wrong."""
-
-    def __init__(self, deactivation_time: int, end_time: int, message: str = ""):
-        super().__init__(message)
-        self.deactivation_time = deactivation_time
-        self.end_time = end_time
-
-    def __str__(self):
-        return f"The deactivation time '{self.deactivation_time}' is wrong. It should be equal to the end time '{self.end_time}'."
-
-
-class DuplicateDeviceFoundError(Exception):
-    """The UE is duplicated in the input file."""
-
-    def __init__(self, device_id: int, device_type: str, message: str = ""):
-        super().__init__(message)
-        self.device_id = device_id
-        self.device_type = device_type
-
-    def __str__(self):
-        return f"The device with id '{self.device_id}' and '{self.device_type}' is duplicated in the input file."
 
 
 class ModelTypeNotImplementedError(Exception):
