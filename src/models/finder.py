5cf00748e835557190ff03fba768b9d4bb82034d
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-09 14:16:36 +0200

    changed base station and vehicle finder to improve performance
    Converting the dataframes to dictionaries provides a good performance boost for the subsequent calls. It takes a few seconds for the conversion but worth it for the overall performance boost.

--- src/models/finder.py
+++ src/models/finder.py
@@ -1,6 +1,7 @@
 import logging
 
 from mesa import Agent
+from numpy import ndarray, array, empty
 from pandas import DataFrame
 
 import src.core.common_constants as cc
@@ -11,71 +12,80 @@ logger = logging.getLogger(__name__)
 
 
 class NearestNBaseStationFinder(Agent):
-    def __init__(self, tower_links_df: DataFrame):
+    def __init__(self, v2b_links_df: DataFrame):
         """
-        Initialize the nearest tower look up model.
+        Initialize the nearest base station look up model.
         """
         super().__init__(0, None)
-        self._base_station_links_df: DataFrame = tower_links_df
+        self._v2b_links_df: DataFrame = v2b_links_df
 
-        self._filtered_base_station_links_df: DataFrame = DataFrame()
+        self._v2b_links_data: dict = {}
+        self._base_station_distances: dict = {}
+
+        self._filtered_v2b_links_data: dict = {}
+        self._filtered_base_station_distances: dict = {}
         self.current_time: int = -1
 
-    def step(self) -> None:
-        """
-        Step through the tower finder.
-        """
-        # Filter the tower links df to only include the current time step
-        self._filtered_base_station_links_df = self._base_station_links_df[
-            self._base_station_links_df[cc.TIME_STEP] == self.current_time
-        ]
+        self._create_v2b_links_data()
 
-    def update_base_station_links(self, tower_links_df: DataFrame) -> None:
+    def _create_v2b_links_data(self) -> None:
+        """
+        Create the v2b links data.
+        """
+        # Convert the v2b links df to dictionary
+        self._v2b_links_data = (
+            self._v2b_links_df.groupby(cc.TIME_STEP)
+            .apply(lambda x: dict(zip(x[cc.VEHICLE_ID], x[cc.BASE_STATIONS])))
+            .to_dict()
+        )
+
+        self._base_station_distances = (
+            self._v2b_links_df.groupby(cc.TIME_STEP)
+            .apply(lambda x: dict(zip(x[cc.VEHICLE_ID], x[cc.DISTANCES])))
+            .to_dict()
+        )
+
+        del self._v2b_links_df
+
+    def update_base_station_links(self, v2b_links_df: DataFrame) -> None:
         """
         Update the base station links.
         """
-        self._base_station_links_df = tower_links_df
+        self._v2b_links_df = v2b_links_df
+        self._create_v2b_links_data()
 
-    def select_n_base_stations_for_vehicle(self, vehicle_id: int, n: int) -> list[int]:
+    def step(self) -> None:
+        """
+        Step through the base station finder.
+        """
+        self._filtered_v2b_links_data.clear()
+        if self.current_time not in self._v2b_links_data:
+            return
+
+        self._filtered_v2b_links_data = self._v2b_links_data[self.current_time]
+        self._filtered_base_station_distances = self._base_station_distances[
+            self.current_time
+        ]
+
+    def select_n_stations_for_vehicle(self, vehicle_id: int, n: int) -> ndarray[int]:
         """
         Select base stations for the vehicle.
         """
-        # Get the base stations for the vehicle.
         logger.debug(f"Looking up base stations for vehicle {vehicle_id}")
-        vehicle_base_stations = self._filtered_base_station_links_df[
-            self._filtered_base_station_links_df[cc.VEHICLE_ID] == vehicle_id
-        ]
 
-        logger.debug(
-            f"Vehicle {vehicle_id} base stations: {vehicle_base_stations[cc.BASE_STATIONS].iloc[0]}"
-        )
-        # Create a dictionary of base stations and their distances from the vehicle base stations df.
-        tower_distances = {
-            tower_id: distance
-            for tower_id, distance in zip(
-                [
-                    int(x)
-                    for x in vehicle_base_stations[cc.BASE_STATIONS].iloc[0].split(" ")
-                ],
-                [
-                    float(x)
-                    for x in vehicle_base_stations[cc.DISTANCES].iloc[0].split(" ")
-                ],
-            )
-        }
+        if len(self._filtered_v2b_links_data) == 0:
+            return empty(0, dtype=int)
 
-        # Sort the tower distances dictionary by distance.
-        sorted_tower_distances = {
-            tower_id: distance
-            for tower_id, distance in sorted(
-                tower_distances.items(), key=lambda item: item[1]
-            )
-        }
+        # Get the base stations for the vehicle.
+        base_stations: str = self._filtered_v2b_links_data[vehicle_id]
+
+        # Split the base stations string into a numpy array of integers.
+        base_stations: ndarray[int] = array(base_stations.split(" ")).astype(int)
 
         # Return the n nearest base stations.
-        if len(sorted_tower_distances) < n:
-            return list(sorted_tower_distances.keys())
-        return list(sorted_tower_distances.keys())[:n]
+        if len(base_stations) < n:
+            return base_stations
+        return base_stations[:n]
 
 
 class TraceVehicleNeighbourFinder(Agent):
@@ -86,57 +96,70 @@ class TraceVehicleNeighbourFinder(Agent):
         super().__init__(0, None)
         self._v2v_links_df: DataFrame = v2v_links_df
 
-        self._filtered_v2v_links_df: DataFrame = DataFrame()
+        self._v2v_links_data: dict = {}
+        self._neighbour_distances: dict = {}
+
+        self._filtered_v2v_links_data: dict = {}
+        self._filtered_neighbour_distances: dict = {}
         self.current_time: int = -1
+
+        self._create_v2v_links_data()
+
+    def _create_v2v_links_data(self) -> None:
+        """
+        Create the v2b links data.
+        """
+        # Convert the v2b links df to dictionary
+        self._v2v_links_data = (
+            self._v2v_links_df.groupby(cc.TIME_STEP)
+            .apply(lambda x: dict(zip(x[cc.VEHICLE_ID], x[cc.NEIGHBOURS])))
+            .to_dict()
+        )
+
+        self._neighbour_distances = (
+            self._v2v_links_df.groupby(cc.TIME_STEP)
+            .apply(lambda x: dict(zip(x[cc.VEHICLE_ID], x[cc.DISTANCES])))
+            .to_dict()
+        )
+
+        del self._v2v_links_df
+
+    def update_base_station_links(self, v2v_links: DataFrame) -> None:
+        """
+        Update the vehicle neighbour links.
+        """
+        self._v2v_links_df = v2v_links
+        self._create_v2v_links_data()
 
     def step(self) -> None:
         """
         Step through the neighbour finder.
         """
-        # Filter the tower links df to only include the current time step
-        self._filtered_v2v_links_df = self._v2v_links_df[
-            self._v2v_links_df[cc.TIME_STEP] == self.current_time
+        self._filtered_v2v_links_data.clear()
+        if self.current_time not in self._v2v_links_data:
+            return
+
+        self._filtered_v2v_links_data = self._v2v_links_data[self.current_time]
+        self._filtered_neighbour_distances = self._neighbour_distances[
+            self.current_time
         ]
 
-    def update_base_station_links(self, v2v_links: DataFrame) -> None:
-        """
-        Update the base station links.
-        """
-        self._v2v_links_df = v2v_links
-
-    def find_vehicles(self, vehicle_id: int) -> list[int]:
+    def find_vehicles(self, vehicle_id: int) -> ndarray[int]:
         """
         Find vehicles that are neighbour for the given vehicle.
         """
-        # It is possible that there are no v2v links for the current time step.
-        if self._filtered_v2v_links_df.empty:
-            return []
+        logger.debug(f"Looking up neighbours for vehicle {vehicle_id}")
+
+        if len(self._filtered_v2v_links_data) == 0:
+            return empty(0, dtype=int)
+
+        if vehicle_id not in self._filtered_v2v_links_data:
+            return empty(0, dtype=int)
 
         # Get the neighbours for the vehicle.
-        neighbour_vehicles = self._filtered_v2v_links_df[
-            self._filtered_v2v_links_df[cc.VEHICLE_ID] == vehicle_id
-        ]
+        neighbours: str = self._filtered_v2v_links_data[vehicle_id]
 
-        # If there are no neighbours, return an empty list.
-        if neighbour_vehicles.empty:
-            return []
+        # Split the neighbours string into a numpy array of integers.
+        neighbours: ndarray[int] = array(neighbours.split(" ")).astype(int)
 
-        # Create a dictionary of vehicles and their distances from the selected vehicle.
-        neighbour_distances = {
-            veh_id: distance
-            for veh_id, distance in zip(
-                [int(x) for x in neighbour_vehicles[cc.NEIGHBOURS].iloc[0].split(" ")],
-                [float(x) for x in neighbour_vehicles[cc.DISTANCES].iloc[0].split(" ")],
-            )
-        }
-
-        # Sort the vehicle distances dictionary by distance.
-        sorted_tower_distances = {
-            veh_id: distance
-            for veh_id, distance in sorted(
-                neighbour_distances.items(), key=lambda item: item[1]
-            )
-        }
-
-        # Return all the neighbours.
-        return list(sorted_tower_distances.keys())
+        return neighbours
