248e4cbe0435c41c6fac97e77031b995e1fdd7d8
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-08-22 20:57:52 +0200

    changed the names to be more specific and added new functions accordingly

--- src/models/composer.py
+++ src/models/composer.py
@@ -32,15 +32,16 @@ class VehicleDataComposer:
         data_source_params : dict
             The data source parameters from the config file.
         """
-        self._all_data_sources: list[DataSource] = []
-        self._side_links_sources: list[DataSource] = []
+        self._v2r_data_sources: list[DataSource] = []
+        self._v2v_data_sources: list[DataSource] = []
+        self._v2b_data_sources: list[DataSource] = []
 
         self.previous_time: int = 0
         self._create_data_sources(data_source_params[ModelName.DATA_SOURCE])
 
     def _create_data_sources(self, data_source_params: dict) -> None:
         """
-        Create the data sources.
+        Creates the data sources for the device.
 
         Parameters
         ----------
@@ -54,54 +55,23 @@ class VehicleDataComposer:
         for params in data_source_params:
             data_source = DataSource()
 
-            data_source.data_type = params[DataSourceKey.DATA_SOURCE_TYPE]
-            data_source.data_counts = params[DataSourceKey.DATA_COUNTS]
-            data_source.data_size = params[DataSourceKey.DATA_SIZE]
-            data_source.data_priority = params[DataSourceKey.DATA_PRIORITY]
-            data_source.side_link = params[DataSourceKey.DATA_SIDE_LINK]
+            data_source.data_type = params[DataSourceKey.SOURCE_TYPE]
+            data_source.data_counts = params[DataSourceKey.COUNTS]
+            data_source.data_size = params[DataSourceKey.SIZE]
+            data_source.data_priority = params[DataSourceKey.PRIORITY]
+            data_source.data_target_type = params[DataSourceKey.TARGET_TYPE]
 
-            self._all_data_sources.append(data_source)
-            if data_source.side_link == "yes":
-                self._side_links_sources.append(data_source)
-
-    def compose_uplink_payload(self, current_time: int) -> VehiclePayload:
-        """
-        Compose uplink payload using all the data sources.
-
-        Parameters
-        ----------
-        current_time : int
-            The current time.
-
-        Returns
-        -------
-        VehiclePayload
-            The uplink payload.
-        """
-        uplink_payload = self.compose_payload_with_sources(
-            current_time, self._all_data_sources
-        )
-        return uplink_payload
-
-    def compose_sidelink_payload(self, current_time: int):
-        """
-        Compose sidelink payload using all the side link data sources.
-
-        Parameters
-        ----------
-        current_time : int
-            The current time.
-
-        Returns
-        -------
-        VehiclePayload
-            The sidelink payload.
-        """
-        sidelink_payload = self.compose_payload_with_sources(
-            current_time, self._side_links_sources
-        )
-        self.previous_time = current_time
-        return sidelink_payload
+            match data_source.data_target_type:
+                case DataTargetType.BASE_STATION:
+                    self._v2b_data_sources.append(data_source)
+                case DataTargetType.ROADSIDE_UNIT:
+                    self._v2r_data_sources.append(data_source)
+                case DataTargetType.VEHICLE:
+                    self._v2v_data_sources.append(data_source)
+                case _:
+                    raise InvalidDataTargetError(
+                        data_source.data_target_type, DeviceName.VEHICLES
+                    )
 
     def compose_payload_with_sources(
         self, current_time: int, data_sources: list[DataSource]
@@ -145,20 +115,82 @@ class VehicleDataComposer:
 
         return vehicle_payload
 
+    def compose_v2b_payload(self, current_time: int) -> VehiclePayload:
+        """
+        Compose v2b payload using all the data sources.
+
+        Parameters
+        ----------
+        current_time : int
+            The current time.
+
+        Returns
+        -------
+        VehiclePayload
+            The v2b payload.
+        """
+        v2b_payload = self.compose_payload_with_sources(
+            current_time, self._v2b_data_sources
+        )
+        return v2b_payload
+
+    def compose_v2v_payload(self, current_time: int) -> VehiclePayload:
+        """
+        Compose v2v payload using the data sources.
+
+        Parameters
+        ----------
+        current_time : int
+            The current time.
+
+        Returns
+        -------
+        VehiclePayload
+            The v2v payload.
+        """
+        v2v_payload = self.compose_payload_with_sources(
+            current_time, self._v2v_data_sources
+        )
+        return v2v_payload
+
+    def compose_v2r_payload(self, current_time: int) -> VehiclePayload:
+        """
+        Compose v2r payload using the data sources.
+
+        Parameters
+        ----------
+        current_time : int
+            The current time.
+
+        Returns
+        -------
+        VehiclePayload
+            The v2r payload.
+        """
+        v2r_payload = self.compose_payload_with_sources(
+            current_time, self._v2r_data_sources
+        )
+        return v2r_payload
+
 
 class BaseStationDataComposer:
-    def __init__(self, model_data: dict):
+    def __init__(self, data_source_params: dict):
         """
         Initialize the data composer.
+
+        Parameters
+        ----------
+        data_source_params : dict
+            The data source parameters from the config file.
         """
-        self.model_data = model_data
+        self.model_data = data_source_params
         self._previous_time: int = 0
 
     def compose_basestation_payload(
         self, current_time: int, incoming_data: dict[int, VehiclePayload]
     ) -> BaseStationPayload:
         """
-        Generate data request by running the applications.
+        Generate the base station payload.
         """
         base_station_payload: BaseStationPayload = BaseStationPayload()
         base_station_payload.timestamp = current_time
